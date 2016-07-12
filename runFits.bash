9d96207451432f270567b2f99a93e5d86e449b80
John <johnmarcampbell@gmail.com>
2016-07-12 11:54:01 -0400

    Refactor fit-running scripts

--- runFits.bash
+++ runFits.bash
@@ -4,10 +4,11 @@ fitFile=$3
 resolution=$4
 q2OrMult=$5
 q2MultBin=$6
+zdcBin=$7
 
 root -b -q 'doCorrectHistogram.C("'$rawFile'","'$correctedFile'",'$resolution')'
-# for((i=3; i <= 3; i++))
+# for((i=0; i <= 0; i++))
 for((i=0; i <= 7; i++))
 do
-    root -b -q 'fitManager.C("'$correctedFile'","'$fitFile'", 0, 0, '$q2OrMult', '$q2MultBin', '$i')'
+    root -b -q 'fitManager.C("'$correctedFile'","'$fitFile'", '$zdcBin', 0, '$q2OrMult', '$q2MultBin', '$i')'
 done
